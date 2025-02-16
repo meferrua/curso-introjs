81d092ab87368b75e60eb3583b5cdcf00a35d3d7
me ferrua <meferrua.ls@gmail.com>
2025-02-16 06:17:29 -0300

    add new comments

--- file.js
+++ file.js
@@ -1,8 +1,10 @@
 //mkdir: crear carpetas
+
 // git satatus: en rojo no esta agregado, en verde si
  // git add .  : etaoa de preparacion PARA AGREGAR
  //  git commit -m "first commit: cambios" : mensaje entre comillas es opcional pero  es necesario agregar descripciones cortas para explicar lo que hace el commit (-M ES PARA AGREGAR LA DESCRIPCION EN LA MISMA LINEA DE COMANDOS)
  // git push origin master : para subirlo a la nube
  // git push -u : (-u) hace que recuerde la configuracion (main/master), los siguientes push no hacen falta el "origin master"
+
  // git clone + url : clona repors (click en "clone" (git hub) para copiar url)
- 
+ // git pull : actualiza un repor ya clonado
