26cd1731b3da99c7c04632f3348aa7ac5f74b05d
me ferrua <meferrua.ls@gmail.com>
2025-02-16 05:43:58 -0300

    add coments

--- file.js
+++ file.js
@@ -1,1 +1,7 @@
 //prueba de modificacion
+// git satatus: en rojo no esta agregado, en verde si
+ // git add .  : etaoa de preparacion PARA AGREGAR
+ //  git commit -m "first commit: cambios" : mensaje entre comillas es opcional pero  es necesario agregar descripciones cortas para explicar lo que hace el commit (-M ES PARA AGREGAR LA DESCRIPCION EN LA MISMA LINEA DE COMANDOS)
+ // git push origin master : para subirlo a la nube
+ 
+ 
